ccf0525ccff676178920c0b29488f65d9aeb8d97
lamchanhiephceet <lamchanhiephceet@gmail.com>
2020-06-09 08:48:12 +0700

    Chuyen_doi_tien.java

--- JAVA/src/Chuyen_doi_tien.java
+++ JAVA/src/Chuyen_doi_tien.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Chuyen_doi_tien {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double usd, cash, vnd = 23000;
+        long usd, cash, vnd = 23000;
         System.out.println("Nhap so tien USD can chuyen:");
         usd = scanner.nextInt();
 
         cash = usd*vnd;
-        System.out.printf("%-20.2f%s", cash, "VND");
+        System.out.printf( cash+ "VND");
     }
 }
